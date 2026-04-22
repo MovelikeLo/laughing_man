079741a5c528c21c4d337bfd22111e3e128b4dc1
MovelikeLo <loicrobert640@gmail.com>
2026-04-22 10:14:14 -0400

    Initial Commit

---
+++ laughin.man.js
@@ -0,0 +1,16 @@
+const SQUARE_COUNT = 3;
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#square").addEventListener("click", () => {
+        alert("You clicked me!");
+    });
+    let box = document.querySelector("#box");
+
+    for(let i = 0; i < SQUARE_COUNT; i++) {
+       let square = document.createElement("img");
+       square.src = "laughing_man.jpg";
+       square.alt = "Laughing Man";
+       square.class = "square";
+       box.appendChild(square);
+    }
+});
